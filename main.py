2b54ff83513ca118dff3ffafb133805f2ef26c0f
Dexit9144 <daniel0453@icloud.com>
2024-02-01 19:52:13 +0100

    error version

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "StarWars"
 
+LASER_SPEED = 10
+
 
 class Falcon(arcade.Sprite):
     def __init__(self):
         super().__init__("falcon.png", 0.3)
-        self.center_x = SCREEN_WIDTH/2
+        self.center_x = SCREEN_WIDTH / 2
         self.center_y = 100
+
+    def update(self):
+        self.center_x += self.change_x
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        pass
 
 
 class Game(arcade.Window):
@@ -17,9 +25,11 @@ class Game(arcade.Window):
         super().__init__(width, height, title)
         self.setup()
         self.bg = arcade.load_texture("background.jpg")
+        self.set_mouse_visible(False)
 
         """Sprites"""
         self.falcon = Falcon()
+        self.laser = Laser()
 
     def setup(self):
         pass
@@ -30,12 +40,32 @@ class Game(arcade.Window):
 
         """Sprites"""
         self.falcon.draw()
+        self.laser.draw()
 
     def update(self, delta_time):
-        pass
+        self.falcon.update()
+        self.laser.update()
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int):
+        self.falcon.center_x = x
 
 
+class Laser(arcade.Sprite):
+    def __init__(self):
+        super().__init__("laser.png", 0.5)
+        self.center_x = window.falcon.center_x
+        self.bottom = window.falcon.top
 
+        self.change_y = LASER_SPEED
+        self.laser_sound = arcade.load_sound("laser.wav")
+
+    def update(self):
+        self.center_y += self.change_y
+
+print(Game.mro())
+print(dir(Game))
 window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+print(dir(window))
 window.setup()
+
 arcade.run()
